17737b6b85b2b7b4ba0ad3834408f9e7dbb037f5
HengFL <HengFL@gmail.com>
2019-10-24 11:44:40 +0700

    Update fl-autoplaceholder.js

--- js/fl-autoplaceholder.js
+++ js/fl-autoplaceholder.js
@@ -3,10 +3,10 @@
     $.fn.flAutoplaceholder = function() {
         var arr = this.serializeArray();
         $.each(arr, function (i,v) {
-            var for = $("[for="+v.name+"]").text();
+            var label = $("[for="+v.name+"]").text();
             var name = $("[name="+v.name+"]").attr("placeholder");
-            if(!jQuery.isEmptyObject(name) && !jQuery.isEmptyObject(for)){
-             $("[name="+v.name+"]").attr("placeholder",for.trim());
+            if(jQuery.isEmptyObject(name) && !jQuery.isEmptyObject(label)){
+             $("[name="+v.name+"]").attr("placeholder",label.trim());
             }
         });
     };
